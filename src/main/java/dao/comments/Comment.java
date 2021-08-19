74368d3203ae0e6bc1e4d12c28671c85fb5607ae
Dymitr_Misiejuk <dymitrmisiejuk@gmail.com>
2021-08-19 17:29:26 +0200

    added repo

--- src/main/java/dao/comments/Comment.java
+++ src/main/java/dao/comments/Comment.java
@@ -1,5 +1,24 @@
 package dao.comments;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Set<Long> userId;
+    private Set<Long> booksId;
 }
