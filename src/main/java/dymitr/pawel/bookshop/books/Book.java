e90e2fa637d42fa7160113ea6245e7e8506c491b
pablo93x <pawel.piatek189@interia.pl>
2021-08-19 16:40:50 +0200

    Book entity update

---
+++ src/main/java/dymitr/pawel/bookshop/books/Book.java
@@ -0,0 +1,18 @@
+package dymitr.pawel.bookshop.books;
+
+import java.math.BigDecimal;
+
+public class Book
+{
+    private long id;
+    private String summary;
+    private String author;
+    private String title;
+    private long isbn;
+    private BigDecimal price;
+    private boolean outOfStockInfo;
+    private  String comments;
+
+
+
+}
