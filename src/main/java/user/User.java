492dc9b9034227d6abcad53cbe35b93303dfb0a7
Dymitr_Misiejuk <dymitrmisiejuk@gmail.com>
2021-08-19 16:35:04 +0200

    User Entity added

---
+++ src/main/java/user/User.java
@@ -0,0 +1,27 @@
+package user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
+   private String login;
+   private String password;
+   private String email;
+   private String address;
+   private List<Book> bookList;
+   private List<Comment> commentList;
+}
