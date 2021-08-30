152fdd8d33ca602427d3f2d3b7f6b70ab6d76478
Dymitr_Misiejuk <dymitrmisiejuk@gmail.com>
2021-08-30 15:58:21 +0200

    Added new comment controller and comment service

---
+++ src/main/java/dymitr/pawel/bookshop/controller/comment/CommentController.java
@@ -0,0 +1,13 @@
+package dymitr.pawel.bookshop.controller.comment;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class CommentController {
+
+
+}
---
+++ src/main/java/dymitr/pawel/bookshop/service/comment/CommentService.java
@@ -0,0 +1,37 @@
+package dymitr.pawel.bookshop.service.comment;
+
+import dymitr.pawel.bookshop.dao.comments.Comment;
+import dymitr.pawel.bookshop.dto.comments.CommentsRequest;
+import dymitr.pawel.bookshop.repository.BooksRepository;
+import dymitr.pawel.bookshop.repository.CommentsRepository;
+import dymitr.pawel.bookshop.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CommentService {
+
+    private final CommentsRepository commentsRepository;
+    private final UsersRepository usersRepository;
+    private final BooksRepository booksRepository;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void createComment(Long userId, Long bookId, CommentsRequest commentsRequest) {
+        Comment comment = new Comment();
+
+        comment.setCommentary(commentsRequest.getCommentary());
+        comment.setUserId(usersRepository.getById(userId));
+        comment.setBooksId(booksRepository.getById(bookId));
+
+        commentsRepository.save(comment);
+        logger.info("comment was created by user: " + usersRepository.getById(bookId).getLogin()
+                + "for book: " + booksRepository.getById(bookId).getTitle());
+    }
+}
